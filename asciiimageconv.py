6141e8f4c3f6a8ef16c7b631608c6f7507790ad5
Alan <alanbince.mec@gmail.com>
2020-04-14 14:52:05 +0530

    Added files

---
+++ asciiimageconv.py
@@ -0,0 +1,20 @@
+import sys
+from PIL import Image
+image_path = sys.argv[0]
+img = Image.open("ole.jpg")
+width, height = img.size
+aspect_ratio = height/width
+new_width = 120
+new_height = aspect_ratio*new_width*0.55
+img = img.resize((new_width, int(new_height)))
+img = img.convert('L')
+pixels = img.getdata()
+chars = ["B","S","#","&","@","$","%","*","!",":","."]
+new_pixels = [chars[pixel//25] for pixel in pixels]
+
+new_pixels = ''.join(new_pixels)
+new_pixels_count= len(new_pixels)
+ascii_image = [new_pixels[index:index+new_width] for index in range(0,new_pixels_count,new_width)]
+ascii_image = "\n".join(ascii_image)
+with open("ascii_image.txt",'w') as f:
+    f.write(ascii_image)
